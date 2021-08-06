49d2fe09156cd51199f6f508ecaf49ec0cd1a832
Sibel Eren <edaeren575@gmail.com>
2021-08-06 17:00:36 +0100

    pull weather from openweather using api

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#temperature').innerText = thermostat.temperature;
     document.querySelector('#temperature').className = thermostat.energyUsage();
   }
-
-  const thermostat = new Thermostat();
-  updateTemperature();
+    const thermostat = new Thermostat();
+      updateTemperature();
 
   document.querySelector('#temperature-up').addEventListener('click', () => {
     thermostat.up();
@@ -32,7 +31,31 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('#powersaving-off').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOff();
 
-    document.querySelector('#power-saving-status').innerText = 'on';
+    document.querySelector('#power-saving-status').innerText = 'off';
     updateTemperature();
   });
+
+
+  const displayWeather = (city) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4e28d62f05f0739bcb585c1cdd05b27d&units=metric`
+
+
+  fetch(url)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      document.querySelector('#current-temperature').innerText = data.main.temp;
+    })
+  }
+  
+
+  document.querySelector('#select-city').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = document.querySelector('#current-city').value;
+
+    displayWeather(city);
+  })
+
+
 });
